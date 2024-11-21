ecde06e407e154e6b1282270f189c03fee9bb9b2
D3vd4th <devadathans01@gmail.com>
2024-11-21 21:47:15 +0530

    initial commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,12 @@
+import {Suspense,useEffect,useState} from 'react'
+import {Canvas} from '@react-three/fiber'
+import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
+import {Canvasloader} from '../Loader'
+
+const Computers = () => {
+  return (
+    <div>Computers</div>
+  )
+}
+
+export default Computers

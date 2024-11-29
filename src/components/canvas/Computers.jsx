64b5fc881e83b7d5c7c9446db721ac5d2daa90fd
D3vd4th <devadathans01@gmail.com>
2024-11-29 13:45:51 +0530

    Computers done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,6 @@
 import {Suspense,useEffect,useState} from 'react'
 import {Canvas} from '@react-three/fiber'
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
-import {Canvasloader} from '../Loader'
 
 const Computers = () => {
   return (

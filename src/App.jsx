bf97ca2344e4e829b864b262d221d0f247ba0578
D3vd4th <devadathans01@gmail.com>
2024-11-29 13:44:37 +0530

    App.js done

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
   return (
-    <div>
+    <div >
       <BrowserRouter>
         <div className="relative z-0  bg-primary">
+          <div className="relative z-0">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
             <Hero />
           </div>
           <About />
+          <StarsCanvas />
+          </div>
+          <div className="relative z-0">
           <Experience />
           <Tech />
+          <StarsCanvas />
+         </div>
           <Works />
           <Feedbacks />
           <div className="relative z-0">
